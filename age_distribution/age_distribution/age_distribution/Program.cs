da5fdfe89732b3b9d9cae3b2ee2fb13481dcbc1b
mcp <mechomeister@gmail.com>
2017-09-29 22:05:08 +0200

    initial project files added

---
+++ age_distribution/age_distribution/age_distribution/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace age_distribution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = File.OpenText(args[0]))
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    if (null == line)
+                        continue;
+                    // do something with line
+                }
+        }
+    }
+}
---
+++ age_distribution/test.c
@@ -0,0 +1,2 @@
+
+bool file
